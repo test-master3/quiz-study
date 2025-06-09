d7abc5d3ae32ec4d47da0e6ff6401fa0bfb72f46
test-master3 <247.sunsunsunday.master@gmail.com>
2025-06-10 08:46:39 +0900

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -13,8 +13,8 @@ bundle exec rake assets:precompile
 
 # Setup database with environment variable
 export DISABLE_DATABASE_ENVIRONMENT_CHECK=1
-bundle exec rake db:schema:load
-bundle exec rake db:migrate
+bundle exec rake db:migrate:reset
 bundle exec rake db:seed
 
+# Start the server
 bundle exec puma -C config/puma.rb 
