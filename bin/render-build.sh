3d1f0d7cd2f5d4fa089560833d76662dd710a480
test-master3 <247.sunsunsunday.master@gmail.com>
2025-06-10 08:17:51 +0900

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -11,7 +11,8 @@ bundle exec rake assets:clean
 # Compile assets
 bundle exec rake assets:precompile
 
-# Setup database
+# Setup database with environment variable
+export DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bundle exec rake db:schema:load
 bundle exec rake db:migrate
 bundle exec rake db:seed
