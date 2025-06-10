5c49f20a2832124d855f75e30b4475982f52e946
test-master3 <247.sunsunsunday.master@gmail.com>
2025-06-10 09:24:44 +0900

    デプロイ用に調整

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,19 +2,7 @@
 # exit on error
 set -o errexit
 
-# Install dependencies
 bundle install
-
-# Clean assets
-bundle exec rake assets:clean
-
-# Compile assets
 bundle exec rake assets:precompile
-
-# Setup database with environment variable
-export DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+bundle exec rake assets:clean
 bundle exec rake db:migrate
-bundle exec rake db:seed
-
-# Start the server
-bundle exec puma -C config/puma.rb 
