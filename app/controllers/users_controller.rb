5ebebc58e27a3fc91f3067e6c2288d90cc71fb5a
test-master3 <247.sunsunsunday.master@gmail.com>
2025-06-20 16:25:18 +0900

    サイドバー、画面遷移の改善

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   # app/controllers/users_controller.rb
   def show
     @user = User.find(params[:id])
+    @quizzes = @user.quizzes.order(created_at: :desc).limit(5)
   end
 end
