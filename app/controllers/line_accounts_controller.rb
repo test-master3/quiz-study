a583c7f79cfded136ae698f924b548a86d3818bc
test-master3 <247.sunsunsunday.master@gmail.com>
2025-06-20 16:44:54 +0900

    line連携状態の表示の変更とサイドバーの改良

--- app/controllers/line_accounts_controller.rb
+++ app/controllers/line_accounts_controller.rb
@@ -2,8 +2,11 @@ class LineAccountsController < ApplicationController
   before_action :authenticate_user!
 
   def new
-    # 現在のユーザーの連携トークンを生成（または再生成）する
-    current_user.generate_line_link_token
-    @link_token = current_user.line_link_token
+    @line_linked = current_user.line_linked?
+    unless @line_linked
+      # 未連携の場合のみ、連携トークンを生成する
+      current_user.generate_line_link_token
+      @link_token = current_user.line_link_token
+    end
   end
 end
