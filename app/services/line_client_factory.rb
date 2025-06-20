336ade561b3f19338ab967ccf4c718daa382103c
test-master3 <247.sunsunsunday.master@gmail.com>
2025-06-20 15:14:37 +0900

    Update line_client_factory.rb

--- app/services/line_client_factory.rb
+++ app/services/line_client_factory.rb
@@ -2,7 +2,7 @@ require 'line/bot'
 
 class LineClientFactory
   def self.build
-    ::Line::Bot::Client.new do |config|
+    Line::Bot::Client.new do |config|
       config.channel_secret = ENV['LINE_CHANNEL_SECRET']
       config.channel_token = ENV['LINE_CHANNEL_TOKEN']
     end
